c4816f35b665d25ebb82b480be20ca98e0d74cdd
velmor99 <volgagaz24100@gmail.com>
2020-06-05 00:05:42 +0300

    try catch error (not done)

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,5 @@
 const BASE = 'https://restcountries.eu/rest/v2/name/';
 
-export default function resApi(search, callback, errorCallback) {
-	fetch(BASE + search).then((j) => j.json()).then((d) => callback(d)).catch(() => errorCallback());
+export default function resApi(search, callback) {
+	fetch(BASE + search).then((j) => j.json()).then((d) => callback(d));
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import resApi from './fetch.js';
 import './styles.css';
 
 const search = document.querySelector('#search_input');
-const baseUrl = 'https://restcountries.eu/rest/v2/name/';
 const resultList = document.querySelector('.result_list');
 const notResult = '<li class="empty">>Result not found :(</li>'
 
@@ -20,7 +19,10 @@ search.addEventListener(
       resultList.innerHTML = "";
       return
     } else  {
-      resApi(event.target.value, renderContent, errorAlert (notResult));
+      try {resApi(event.target.value, renderContent);
+      } catch {
+          resultList.innerHTML = notResult;
+      }
     }
 	}, 500)
 );
@@ -29,15 +31,12 @@ function renderContent(d) {
   if (d.length === 1) {
 		const murcup = d.map((item) => firstResult(item)).join('');
 		resultList.innerHTML = murcup;
-	} else if (d.length < 10) {
+	} else if (d.length > 1 && d.length <= 10) {
 		const secondMurcup = d.map((item) => secondResult(item)).join('');
 		resultList.innerHTML = secondMurcup;
-	} else if (d.length === 0 || d.length > 10) {
+	} else if (d.length > 10) {
 		resultList.innerHTML = '';
 		alert('Too many matches found. Please enter a more specific query!');
   }
 }
 
-  function errorAlert (result) {
-    resultList.innerHTML = result;
-  }
